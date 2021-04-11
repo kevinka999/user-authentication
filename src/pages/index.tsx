49c12b6b6ece80fe1428b5ca3ac68abe9c9cef8a
kevinka999 <kevinka999@gmail.com>
2021-04-10 22:25:14 -0300

    Estrutura e criação dos componentes base

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Register, Login, Main} from '../components/';
+
+export default function Home() {
+  return (
+    <div>
+      <Register />
+      <Login />
+      <Main />
+    </div>
+  )
+}
